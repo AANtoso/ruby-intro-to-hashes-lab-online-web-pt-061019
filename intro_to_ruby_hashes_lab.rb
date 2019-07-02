09ce9d15cffd35922a9765e1434912baac235c43
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-02 17:22:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ return monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly_with_second_tier = {railroads: {:pieces=>4,:names=>{}
+	monopoly_with_second_tier = {railroads: {:pieces=>4,:names=>{},:rent_in_dollars=>{}
     }
 	}
 	return monopoly_with_second_tier
