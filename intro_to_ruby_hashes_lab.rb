4326e56e835cdda5859ab32c3b10d47a711f561a
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-02 17:24:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly_with_third_tier = {railroads: {:pieces=>4,:names=>{},:rent_in_dollars=>{:one_piece_owned=>25}
+	monopoly_with_third_tier = {railroads: {:pieces=>4,:names=>{},:rent_in_dollars=>{:one_piece_owned=>25,:two_pieces_owned=>50}
     }
 	}
 	return monopoly_with_third_tier
