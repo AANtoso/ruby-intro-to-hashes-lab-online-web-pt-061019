bfa42e3beee706e000ab8fae28a975e64334bdc1
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-02 17:06:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,8 +21,8 @@ def monopoly_with_second_tier
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
-    }
-	}, {:names=>{}}
+    },:names=>{}
+	}
 }
 	return monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
