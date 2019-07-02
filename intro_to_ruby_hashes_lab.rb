e2cd95dec78673a40293e0c4752efb53ae3c5c44
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-02 17:11:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,6 +18,7 @@ def monopoly_with_second_tier
     },:rent_in_dollars => {},:names=>{}
 	}
 }
+end
 	return monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 end
